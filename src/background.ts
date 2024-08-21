152d11101f4ae6cbc675f480f604ef092b62a576
Francesco Novy <francesco.novy@sentry.io>
2024-08-21 13:17:26 +0200

    fix it & cleanup

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,4 @@
 import browser from 'webextension-polyfill';
-import { getMessageData, isEnvelopeMessage } from './utils/getMessageData';
 
 browser.runtime.onInstalled.addListener((details) => {
 	console.log('Extension installed:', details);
--- src/pages/spotlight.tsx
+++ src/pages/spotlight.tsx
@@ -1,22 +1,51 @@
 import * as SpotlightJs from '@spotlightjs/overlay';
 import browser from 'webextension-polyfill';
 import { getMessageData, isEnvelopeMessage } from '../utils/getMessageData';
+import { onCleanup } from 'solid-js';
+
+const SPOTLIGHT_SELECTOR = '#sentry-spotlight-root';
 
 export default function Spotlight() {
-	SpotlightJs.init({
-		fullPage: true,
-		injectImmediately: true,
-		showTriggerButton: false,
-		integrations: [SpotlightJs.sentry({ injectIntoSDK: false })],
-		showClearEventsButton: false,
-	});
+	const existingSpotlight = document.querySelector(SPOTLIGHT_SELECTOR);
 
-	browser.runtime.onMessage.addListener((message, _sender, sendResponse) => {
-		const data = getMessageData(message);
+	if (!existingSpotlight) {
+		SpotlightJs.init({
+			fullPage: true,
+			injectImmediately: true,
+			showTriggerButton: false,
+			integrations: [
+				SpotlightJs.sentry({
+					injectIntoSDK: false,
+					sidecarUrl: '',
+				}),
+			],
+			showClearEventsButton: false,
+			sidecarUrl: '',
+			skipSidecar: true,
+		});
 
-		if (isEnvelopeMessage(data)) {
-			SpotlightJs.trigger('sentry:addEnvelope', data.envelope);
-			sendResponse();
+		const listener = ((message, sender, sendResponse) => {
+			if (sender.tab?.id !== browser.devtools.inspectedWindow.tabId) {
+				return;
+			}
+
+			const data = getMessageData(message);
+
+			if (isEnvelopeMessage(data)) {
+				SpotlightJs.trigger('sentry:addEnvelope', data.envelope);
+				sendResponse();
+			}
+		}) satisfies Parameters<typeof browser.runtime.onMessage.addListener>[0];
+
+		browser.runtime.onMessage.addListener(listener);
+	} else {
+		existingSpotlight.classList.remove('force-hidden');
+	}
+
+	onCleanup(() => {
+		const element = document.querySelector('#sentry-spotlight-root');
+		if (element) {
+			element.classList.add('force-hidden');
 		}
 	});
 
